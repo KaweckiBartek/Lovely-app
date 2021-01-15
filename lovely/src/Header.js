dfd37190d44a155cd7445dcb68fa3ea80b7e40f8
KaweckiBartek <67326141+KaweckiBartek@users.noreply.github.com>
2021-01-15 12:04:38 +0100

    header ready

--- lovely/src/Header.js
+++ lovely/src/Header.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton'
+
 
 const Header = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className="header__wrapper">
+      <IconButton>
+        <PersonIcon className="header__icon" fontSize="large" />
+      </IconButton>
+      <h1>Logo</h1>
+      <IconButton>
+        <ForumIcon className="header__icon" fontSize="large" />
+      </IconButton>
     </div>
   )
 }
